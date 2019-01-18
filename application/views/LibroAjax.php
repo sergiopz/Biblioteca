1006abe0c235fa48d42874dd90dbaca456e0886c
sergiopz <sergio.perico16@gmail.com>
2019-01-18 09:50:08 +0100

    insercion
    Fin de tabla relacionada de Instituto y su comprobacion ademas de la funcion getAll en Usercontroler

--- application/views/LibroAjax.php
+++ application/views/LibroAjax.php
@@ -46,22 +46,27 @@ echo"
            echo form_open("Libros/ModificarLibro");
            echo "
            <div class='info'>
-           <input type='text' name='id' value='$libro->id'>
-           <input type='text' name='isbn' value='$libro->isbn'>
-           <input type='text' name='titulo' value='$libro->titulo'>
-           <input type='text' name='descripcion' value='$libro->descripcion'>
-           <input type='text' name='fecha' value='$libro->fecha'>
-           <input type='text' name='paginas' value='$libro->paginas'>";
+              <input type='text' name='id' value='$libro->id'>
+              <input type='text' name='isbn' value='$libro->isbn'>
+              <input type='text' name='titulo' value='$libro->titulo'>
+              <input type='text' name='descripcion' value='$libro->descripcion'>
+              <input type='text' name='fecha' value='$libro->fecha'>
+              <input type='text' name='paginas' value='$libro->paginas'>
+                     <select name='idInstituto'>";
            for ($j = 0; $j < count($listaInstitutos); $j++) {
               $instituto = $listaInstitutos[$j]; 
-              if( $libro->idInstituto==$instituto->id ){ 
-              echo "<input type='text' name='nombre' value='$instituto->nombre'>" ;
+              
+                     if( $libro->idInstituto==$instituto->id ){ 
+                            echo "<option  value='$instituto->id' selected >$instituto->nombre</option> ";                      
+                     }else{
+                            echo "<option  value='$instituto->id' >$instituto->nombre</option> ";
+                     }
               }
-          }
-     echo "<input type='text' name='idUsuario' value='$libro->idUsuario'>
-           <input type='text' name='idEditorial' value='$libro->idEditorial'>
-           <input type='hidden' name='do' value='ModificarPeliculas' />
-           <input type='Submit' name='Modificar' value='Modificar'/>
+          echo "     </select>
+              <input type='text' name='idUsuario' value='$libro->idUsuario'>
+              <input type='text' name='idEditorial' value='$libro->idEditorial'>
+              <input type='hidden' name='do' value='ModificarPeliculas' />
+              <input type='Submit' name='Modificar' value='Modificar'/>
                 
            " ;
 
@@ -71,6 +76,14 @@ echo"
              </form>
              ";
        }
+       /*
+
+       echo "<input type='text' name='nombre' value='$instituto->nombre'>" ;
+              echo "<select><option value='a'selected>n</option></select> ";
+       <option value='".$rol['id_rol']."'selected>".$rol['rol_descripcion']."</option> en MVC
+       <option value='".$rol['id_rol']."'>".$rol['rol_descripcion']."</option> en MVC
+       
+       */ 
        
        /*
        for ($i = 0; $i < count($listaInstitutos); $i++) {
