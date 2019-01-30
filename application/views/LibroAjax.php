5b2a77a2690373e6600c97620a720c43dfc7e329
sergiopz <sergio.perico16@gmail.com>
2019-01-30 11:55:44 +0100

    vistaLibro

--- application/views/LibroAjax.php
+++ application/views/LibroAjax.php
@@ -1,4 +1,6 @@
 <script type="text/javascript" src="<?php echo base_url(); ?>js/jqueryMaterialize.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>js/jqueryMaterialize.js"></script>
+<link rel="stylesheet" href="<?php echo base_url('css/estiloMaterialize.css');?>">
 <script> $("document").ready(function() {
             $(".libro").removeClass("#8c9eff indigo accent-1").addClass("#304ffe indigo accent-4");
           });
@@ -10,8 +12,10 @@
        echo  "<div class='formularioInsercion' style='display:inline'>
               <h1>Insertar un Libro</h1>";
        
-       //Aqui comienza el formulario de insercion
+       
+
        echo form_open_multipart("Libros/InsertarLibro");
+       
 
        echo "        <fieldset>
                             Isbn : <input type='text' name='isbn'/><br/>
@@ -65,6 +69,119 @@
               <span>nombre</span>  <br><br>";   
        
        //Aqui comienza la creacion de las tablas
+       ?>
+       <table class="highlight responsive-table #536dfe indigo accent-2 ">
+              <thead>
+                     <tr class="#536dfe indigo accent-2">
+                            <th class="#000000 black-text">Isbn</th>
+                            <th class="#000000 black-text">Titulo</th>
+                            <th class="#000000 black-text">Descripcion</th>
+                            <th class="#000000 black-text">Fecha</th>
+                            <th class="#000000 black-text">Paginas</th>
+                            <th class="#000000 black-text">Instituto</th>
+                            <th class="#000000 black-text">Usuario</th>
+                            <th class="#000000 black-text">Editorial</th>
+                            <th class="#000000 black-text">Autor</th>
+                            <th class="#000000 black-text">Categoria</th>
+                            <th><a href="#insert" class="btn btn-large pulse #00e676 green accent-3 modal-trigger"><i class="material-icons" title="Insertar">add_box</i></a></th>
+                     </tr>
+              </thead>
+              <tbody>
+              <?php
+       for ($i = 0; $i < count($listaLibros); $i++) {
+              $libro = $listaLibros[$i];
+
+       echo form_open("Libros/ModificarLibro");
+       echo   "<div class='info'>
+              <tr class='$libro->id'>
+                     <input type='hidden' name='id' value='$libro->id' readonly>
+                     <td><input class='#ffffff white-text' type='text' name='isbn' value='$libro->isbn'></td>
+                     <td><input class='#ffffff white-text' type='text' name='titulo' value='$libro->titulo'></td>
+                     <td><input class='#ffffff white-text' type='text' name='descripcion' value='$libro->descripcion'></td>
+                     <td><input class='#ffffff white-text' type='text' name='fecha' value='$libro->fecha'></td>
+                     <td><input class='#ffffff white-text' type='text' name='paginas' value='$libro->paginas'></td>
+                     <td><select name='idInstituto'>";
+       for ($j = 0; $j < count($listaInstitutos); $j++) {
+              $instituto = $listaInstitutos[$j]; 
+              if( $libro->idInstituto==$instituto->id ){ 
+       echo          "<option  value='$instituto->id' selected >$instituto->nombre</option> ";                      
+              }else{
+       echo          "<option  value='$instituto->id' >$instituto->nombre</option> ";
+              }
+       }
+
+       echo  "</select></td>";
+          
+       for ($j = 0; $j < count($listaAdministradores); $j++) {
+              $administrador = $listaAdministradores[$j]; 
+              if( $libro->idUsuario==$administrador->id ){ 
+       echo          "<td><input class='#ffffff white-text' type='text'  value='$administrador->nombre' readonly></td>";
+       echo          "<input class='#ffffff white-text' type='hidden' name='idUsuario' value='$administrador->id' />";                      
+              }
+       }
+
+       echo   "<td><select name='idEditorial'>";  
+
+       for ($j = 0; $j < count($listaEditoriales); $j++) {
+              $editorial = $listaEditoriales[$j]; 
+              if( $libro->idEditorial==$editorial->id ){ 
+       echo          "<option  value='$editorial->id' selected >$editorial->nombre</option> ";                      
+              }else{
+       echo          "<option  value='$editorial->id' >$editorial->nombre</option> ";
+              }
+       }
+
+       echo   "</select></td>
+               <td><select required multiple name='idAutor[]'>";
+
+
+       for ($j = $cont=0; $j < count($listaAutores); $j++) {
+              $autor = $listaAutores[$j];
+              for ($k = 0; $k < count($listaAutoresLibros); $k++) {
+                      $autorlibro = $listaAutoresLibros[$k];
+                            if(($autorlibro->idAutor==$autor->id)&&($autorlibro->idLibro==$libro->id)){
+       echo          "<option  value='$autor->id' selected >$autor->nombre</option> "; 
+                            $k=count($listaAutoresLibros); 
+                     }else if ($k==count($listaAutoresLibros)-1)  {
+       echo          "<option  value='$autor->id'  >$autor->nombre</option> ";
+                            $k=count($listaAutoresLibros);
+                     }
+              }
+       }
+      
+       echo   "</select></td>
+               <td><select required multiple name='idCategoria[]'>";
+       for ($j = 0; $j < count($listaCategorias); $j++) {
+                     $categoria = $listaCategorias[$j];
+              for ($k = 0; $k < count($listaLibrosCategorias); $k++) {
+                     $librocategoria = $listaLibrosCategorias[$k];
+                            if(($librocategoria->idCategoria==$categoria->id)&&($librocategoria->idLibro==$libro->id)){
+       echo          "<option  value='$categoria->id' selected >$categoria->nombre</option> "; 
+                            $k=count($listaLibrosCategorias);
+                            }else if($k==count($listaLibrosCategorias)-1) {
+       echo          "<option  value='$categoria->id'>$categoria->nombre</option> ";
+                            $k=count($listaLibrosCategorias);
+                     }
+              }
+       }
+
+   
+       
+       
+       
+      
+       echo   "</select></td>
+                     <td><button class='btn waves-effect waves-light #e65100 orange darken-4 z-depth-0 clasemodificar' value='$libro->id' type='submit' name='action'>Modificar<i class='material-icons right'>create</i></button></td>
+                     <td><a value='$libro->id' class='btn-flat waves-effect waves-light #d32f2f  red darken-2 white-text borrarInstituto' >Eliminar<i class='material-icons right' title='Eliminar'>delete</i></a><td>      
+             </div>
+
+             </form>";
+       } 
+       ?>
+
+              </tbody>
+       </table>
+       <?php
        for ($i = 0; $i < count($listaLibros); $i++) {
               $libro = $listaLibros[$i];
 
@@ -152,7 +269,7 @@
 
        echo         "<button><a href='".site_url('Libros/EliminarLibro/'.$libro->id)."'>Eliminar</a></button>
              </div>
-
+              </tr>
              </form>";
        } 
 
