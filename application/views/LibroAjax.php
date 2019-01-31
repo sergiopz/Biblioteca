98262fc12c655b6ba38ac88541df079887f95d82
sergiopz <sergio.perico16@gmail.com>
2019-01-31 13:42:00 +0100

    Update LibroAjax.php

--- application/views/LibroAjax.php
+++ application/views/LibroAjax.php
@@ -1,33 +1,35 @@
 <script type="text/javascript" src="<?php echo base_url(); ?>js/jqueryMaterialize.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>js/jqueryMaterialize.js"></script>
 <link rel="stylesheet" href="<?php echo base_url('css/estiloMaterialize.css');?>">
-<script> $("document").ready(function() {
-            $(".libro").removeClass("#8c9eff indigo accent-1").addClass("#304ffe indigo accent-4");
-          });
+<script>
+$("document").ready(function() {
+    $(".libro").removeClass("#8c9eff indigo accent-1").addClass("#304ffe indigo accent-4");
+});
 </script>
 <div class="row"></div>
-    <div class="row">
-      <div class="col s12 m12 #536dfe indigo accent-2 z-depth-1 " id="capaAdmin">
-      <!-- Hasta aqui los divs de VistaAdministrador-->
+<div class="row">
+    <div class="col s12 m12 #536dfe indigo accent-2 z-depth-1 " id="capaAdmin">
+        <!-- Hasta aqui los divs de VistaAdministrador-->
 
-       <table class="highlight responsive-table #536dfe indigo accent-2 ">
-              <thead>
-                     <tr class="#536dfe indigo accent-2">
-                            <th class="#000000 black-text">Isbn</th>
-                            <th class="#000000 black-text">Titulo</th>
-                            <th class="#000000 black-text">Descripcion</th>
-                            <th class="#000000 black-text">Fecha</th>
-                            <th class="#000000 black-text">Paginas</th>
-                            <th class="#000000 black-text">Instituto</th>
-                            <th class="#000000 black-text">Usuario</th>
-                            <th class="#000000 black-text">Editorial</th>
-                            <th class="#000000 black-text">Autor</th>
-                            <th class="#000000 black-text">Categoria</th>
-                            <th><a href="#insert" class="btn btn-large pulse #00e676 green accent-3 modal-trigger"><i class="material-icons" title="Insertar">add_box</i></a></th>
-                     </tr>
-              </thead>
-              <tbody>
-              <?php
+        <table class="highlight responsive-table #536dfe indigo accent-2 ">
+            <thead>
+                <tr class="#536dfe indigo accent-2">
+                    <th class="#000000 black-text">Isbn</th>
+                    <th class="#000000 black-text">Titulo</th>
+                    <th class="#000000 black-text">Descripcion</th>
+                    <th class="#000000 black-text">Fecha</th>
+                    <th class="#000000 black-text">Paginas</th>
+                    <th class="#000000 black-text">Instituto</th>
+                    <th class="#000000 black-text">Usuario</th>
+                    <th class="#000000 black-text">Editorial</th>
+                    <th class="#000000 black-text">Autor</th>
+                    <th class="#000000 black-text">Categoria</th>
+                    <th><a href="#insert" class="btn btn-large pulse #00e676 green accent-3 modal-trigger"><i
+                                class="material-icons" title="Insertar">add_box</i></a></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
        for ($i = 0; $i < count($listaLibros); $i++) {
               $libro = $listaLibros[$i];
 
@@ -123,69 +125,59 @@
        } 
        ?>
 
-              </tbody>
-       </table>
-       </div>
-       </div>
+            </tbody>
+        </table>
 
-       <div id="insert" class="modal" style="overflow-y: scroll">
-              <?php    echo form_open_multipart("Administrador/InsertarUsuarios");?>
-              <h5 class="modal-close">&#10005;</h5>
-                     <div class="modal-content center">
-                     <h4 class="flow-text #00e676 green-text text-accent-3">Insertar Registro</h4>
-                            <div class="input-field">
-                                   <i class="material-icons prefix" style="color:royalblue">add_box</i>
-                                   <input type='text' name='isbn' id='isbn'>
-                                   <label style="color:royalblue" for="isbn">isbn</label>
-                            </div>
-                            <div class="input-field">
-                                   <i class="material-icons prefix" style="color:royalblue">add_box</i>
-                                   <input type='text' name='titulo' id='titulo'>
-                                   <label style="color:royalblue" for="titulo">Titulo</label>
-                            </div>
-                            <div class="input-field">
-                                   <i class="material-icons prefix" style="color:royalblue">add_box</i>
-                                   <input type='text' name='descripcion' id='descripcion'>
-                                   <label style="color:royalblue" for="descripcion">Descripcion</label>
-                            </div>                        
-                            <div class="input-field">
-                                   <i class="material-icons prefix" style="color:royalblue">add_box</i>
-                                   <input type='text' name='fecha' id='fecha'>
-                                   <label style="color:royalblue" for="fecha">Fecha</label>
-                            </div>
-                            <div class="input-field">
-                                   <i class="material-icons prefix" style="color:royalblue">add_box</i>
-                                   <input type='text' name='paginas' id='paginas'>
-                                   <label style="color:royalblue" for="paginas">Paginas</label>
-                            </div>
-                            <div class="input-field">
-                                   <i class="material-icons prefix" style="color:royalblue">add_box</i>
-                                          <select name="idInstituto" id="idInstituto">
-                                          <?php
+        <div id="insert" class="modal" style="overflow-y:scroll">
+            <?php    echo form_open_multipart("Administrador/InsertarUsuarios");?>
+            <h5 class="modal-close">&#10005;</h5>
+            <div class="modal-content center">
+                <h4 class="flow-text #00e676 green-text text-accent-3">Insertar Registro</h4>
+                <div class="input-field">
+                    <i class="material-icons prefix" style="color:royalblue">add_box</i>
+                    <input type='text' name='isbn' id='isbn'>
+                    <label style="color:royalblue" for="isbn">isbn</label>
+                </div>
+                <div class="input-field">
+                    <i class="material-icons prefix" style="color:royalblue">add_box</i>
+                    <input type='text' name='titulo' id='titulo'>
+                    <label style="color:royalblue" for="titulo">Titulo</label>
+                </div>
+                <div class="input-field">
+                    <i class="material-icons prefix" style="color:royalblue">add_box</i>
+                    <input type='text' name='descripcion' id='descripcion'>
+                    <label style="color:royalblue" for="descripcion">Descripcion</label>
+                </div>
+                <div class="input-field">
+                    <i class="material-icons prefix" style="color:royalblue">add_box</i>
+                    <input type='text' name='fecha' id='fecha'>
+                    <label style="color:royalblue" for="fecha">Fecha</label>
+                </div>
+                <div class="input-field">
+                    <i class="material-icons prefix" style="color:royalblue">add_box</i>
+                    <input type='text' name='paginas' id='paginas'>
+                    <label style="color:royalblue" for="paginas">Paginas</label>
+                </div>
+                <div class="input-field">
+                    <i class="material-icons prefix" style="color:royalblue">add_box</i>
+                    <select name="idInstituto" id="idInstituto">
+                        <?php
                                           for ($j = 0; $j < count($listaInstitutos); $j++) {
                                            $instituto = $listaInstitutos[$j];
-                                                 if( $usuario->id==$instituto->id ){ 
+                                                 if($j==0){
                                           echo      "<option  value='$instituto->id' selected >$instituto->nombre</option> ";                      
                                                   }else{
-                                          echo      "<option  value='$instituto->id' >$instituto->nombre</option> ";
-                                                 }
+                                          echo      "<option  value='$instituto->id'>$instituto->nombre</option> ";                  
+                                                  }
                                           }?>
-                                          </select>
-                                          <label style="color:royalblue" for="Id Instituto">IdInstituto</label>
-                            </div>
-                            
-                     </div>
+                    </select>
+                    <label style="color:royalblue" for="IdInstituto">IdInstituto</label>
+                </div>
+            </div>
 
-              </form>
-       </div>
-       
+            </form>
+        </div>
 
-       
-       
-       
-
-
-       <!--Esto cierra Vista Administrador-->
-      </div>  
-  </div>
-             
+        <!--Esto cierra Vista Administrador-->
+    </div>
+</div>
