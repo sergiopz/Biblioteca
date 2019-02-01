f2d42369cf94285d80c22676ab6853d255c47764
JuapiCallejon <juapicallejon@gmail.com>
2019-02-01 10:27:51 +0100

    datatable
    lineas de Script datatable

--- application/views/footer.php
+++ application/views/footer.php
@@ -25,5 +25,6 @@
     </footer>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
 </body>
 </html>
