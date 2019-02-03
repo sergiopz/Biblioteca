7294411c7afa8e44ab1c3d2eb85d2b6111a84506
JuapiCallejon <juapicallejon@gmail.com>
2019-02-03 20:40:52 +0100

    Revert "direccion cp"
    This reverts commit 5c3a6ba419000e61681950c5ee737074239b9079.

--- application/views/institutosAjax.php
+++ application/views/institutosAjax.php
@@ -55,13 +55,14 @@
 <table id="table_id" class="display">
     <thead>
         <tr>
-            <th>Nombre</th>
-            <th>Provincia</th>
-            <th>Localidad</th>
-            <th>Dirección/th>
-            <th>Código postal</th>
-            <th>Modificar</th>
-            <th>Eliminar</th>
+            <th>Column 1</th>
+            <th>Column 2</th>
+            <th>Column 3</th>
+            <th>Column 4</th>
+              <th>Column 5</th>
+               <th>Column 6</th>
+               <th>Column 7</th>
+
         </tr>
     </thead>
     <tbody>
@@ -75,7 +76,7 @@
 
             <td><input  class='#ffffff ' type='text' name='localidad'value='$instituto->localidad'></td>
            <td><input  class='#ffffff' type='text' name='direccion'value='$instituto->direccion'></td>
-                  <td><input  class='#ffffff' type='text' name='direccion'value='$instituto->cp'></td>
+                  <td><input  class='#ffffff' type='text' name='direccion'value='$instituto->direccion'></td>
            
 
 
