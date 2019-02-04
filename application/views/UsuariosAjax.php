0610c45108512a03a36e030b41988c814dbd878c
graciancristales <graciancristales@hotmail.com>
2019-02-04 12:21:26 +0100

    tabla 2.0

--- application/views/UsuariosAjax.php
+++ application/views/UsuariosAjax.php
@@ -36,14 +36,13 @@ $(".claseBorrar").click(function() {
       var tipousuario=$("."+iddiv+ " input[name='tipo']").val();
       var idInstitutousuario=$("."+iddiv+ " option:selected").val();
 
+      $("."+iddiv+ " p[name='nombre']").text(nombreusuario);
 
-      $("."+iddiv+ " input[name='nombre']").attr("value",nombreusuario);
-      $("."+iddiv+ " input[name='apellidos']").attr("value",apellidousuario);
-      $("."+iddiv+ " input[name='nick']").attr("value",nickusuario);
-      $("."+iddiv+ " input[name='contrasena']").attr("value",contrasenausuario); 
-      $("."+iddiv+ " input[name='correo']").attr("value",correousuario);
-      $("."+iddiv+ " input[name='telefono']").attr("value",telefonousuario);
-      $("."+iddiv+ " input[name='tipo']").attr("value", tipousuario);
+   
+      $("."+iddiv+ " p[name='apellidos']").text(apellidousuario);
+      $("."+iddiv+ " p[name='nick']").text(nickusuario);
+ 
+      $("."+iddiv+ " p[name='tipo']").text( tipousuario);
       $("."+iddiv+ " option:selected").attr("value",idInstitutousuario);
 
 
@@ -66,64 +65,69 @@ $(".claseBorrar").click(function() {
   //para modificar cogemos todos los campos uno a uno y los guardamos en variables , para enviar todos los datos al controlador por ajax.
  
        
-</script>      
+</script> 
+
+<script>
+  $(document).ready( function () {
+    $('#table_id').DataTable({
+      "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
+        }
+    });
+
+
+
+
+} );
+</script>
+
+
+
      
 
-    
-
- 
-
-
-
-
-
-<table class="highlight responsive-table #536dfe indigo accent-2 ">
-  <thead>
-    <tr class="#536dfe indigo accent-2">
-      <th class="#000000 black-text">Nombre</th>
-      <th class="#000000 black-text">Apellidos</th>
-      <th class="#000000 black-text">Nick</th>
-      <th class="#000000 black-text">Contraseña</th>
-      <th class="#000000 black-text">E-Mail</th>
-      <th class="#000000 black-text">Teléfono</th>
-      <th class="#000000 black-text">Tipo</th>
-      <th class="#000000 black-text">Instituto</th>
-      <th><a href="#insert" class="btn btn-large pulse #00e676 green accent-3 modal-trigger"><i class="material-icons" title="Insertar">add_box</i></a></th>
-    </tr>
-  </thead>
-  <tbody>
-            
-  <?php
-    for ($i = 0; $i < count($listaUsuarios); $i++) {
+    <table id="table_id" class="display">
+    <thead>
+        <tr>
+            <th class="#000000 black-text">Nombre</th>
+            <th class="#000000 black-text">Apellidos</th>
+            <th class="#000000 black-text">Nick</th>
+            <th class="#000000 black-text">Tipo</th>
+            <th class="#000000 black-text">Instituto</th>
+            <th>Modificar</th>
+            <th>Eliminar</th>
+        </tr>
+        
+    </thead>
+    <tbody>
+      <?php
+  for ($i = 0; $i < count($listaUsuarios); $i++) {
           $usuario = $listaUsuarios[$i];
-            echo "<div class='info'>
-              <tr class='$usuario->id'>
-                <input type='text' hidden name='id' value='$usuario->id'>
-                <td><input class='#ffffff white-text' type='text' name='nombre' value='$usuario->nombre'></td>
-                <td><input class='#ffffff white-text' type='text' name='apellidos'value='$usuario->apellidos'></td>
-                <td><input class='#ffffff white-text' type='text' name='nick'value='$usuario->nick'></td>
-                <td><input class='#ffffff white-text' type='text' name='contrasena'value='$usuario->contrasena'></td>
-                <td><input class='#ffffff white-text' type='text' name='correo'value='$usuario->correo'></td>
-                <td><input class='#ffffff white-text' type='text' name='telefono'value='$usuario->telefono'></td>
-                <td><input class='#ffffff white-text' type='text' name='tipo'value='$usuario->tipo'></td>
-                <td><select name='idInstituto'>";
-                for ($j = 0; $j < count($listaInstitutos); $j++) {
-                  $instituto = $listaInstitutos[$j];
-                  if( $usuario->idInstituto==$instituto->id ){ 
-                    echo"<option  value='$instituto->id' selected>$instituto->nombre</option> ";
-                  }else{
-                    echo"<option  value='$instituto->id' >$instituto->nombre</option> ";
-                  }
-                }
-                echo"</select></td>
 
-                <td><button href='#lupa$usuario->id' class='btn waves-effect waves-light #9fa8da indigo lighten-3 z-depth-0 modal-trigger'><i class='material-icons' title='Insertar'>info</i></button></td>
-                <td><button class='btn waves-effect waves-light #e65100 orange darken-4 z-depth-0 claseModificar' value='$usuario->id' type='submit' name='action'><i class='material-icons '>create</i></button></td>
-                <td><a value='$usuario->id' class='btn-flat waves-effect waves-light #d32f2f  red darken-2 white-text claseBorrar' ><i class='material-icons ' title='Eliminar'>delete</i></a></td>
+        echo" <tr class='$usuario->id'>
+                
+                <td><p class='#ffffff black-text' name='nombre'>$usuario->nombre</p></td>
+                     <td><p class='#ffffff black-text' name='apellidos'>$usuario->apellidos</p></td>
+                          <td><p class='#ffffff black-text' name='nick'>$usuario->nick</p></td>
+                               <td><p class='#ffffff black-text' name='tipo'>$usuario->tipo</p></td>
 
-                </tr>
-            </div>
-          ";
+              
+                <td><select class='#ffffff name='idInstituto'>
+
+
+
+
+
+
+             <td><button href='#lupa$usuario->id'  class='btn waves-effect waves-light #9fa8da indigo lighten-3 z-depth-0 modal-trigger anchuraBoton'>Modificar<i class='material-icons right' title='Modificar'>Modificar</i></button></td>
+          
+
+           <td class='anchuraBoton3'><button class='btn-flat waves-effect waves-light #d32f2f  red darken-2 white-text anchuraBoton2 claseBorrar ' value='$usuario->id' type='submit' name='action'>Eliminar<i class='material-icons right' title='Eliminar'>delete</i></button></td>
+            
+            
+        
+
+        </tr>";
+
 
 
           echo"<div id='lupa$usuario->id' class='modal $usuario->id' style='overflow-y: scroll;max-height: 450px;'>
@@ -198,15 +202,21 @@ $(".claseBorrar").click(function() {
             </form>
           </div>
         </div>
-          ";
-
-
-    }
-    ?>
-
-
+        
+     
+   
+       ";
+      }
+      ?>
     </tbody>
-  </table>
+</table>
+ 
+
+ 
+
+
+
+
 
       </div>
     </div>
