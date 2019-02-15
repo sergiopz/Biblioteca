d92ae4272e70f96173cc74999ba1d380d5bc757c
sergiopz <sergio.perico16@gmail.com>
2019-02-15 10:22:46 +0100

    fotos y tal
    visor cambios y css

--- application/views/Visor.php
+++ application/views/Visor.php
@@ -1,64 +1,27 @@
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Visor Vistar</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Estilos del css del visor a continuacion-->
-  
-  <!-- Estilos del css del BookBlock a continuacion-->
-  <link rel="stylesheet" href="<?php echo base_url('css/BookBlock/default.css');?>">
-  <link rel="stylesheet" href="<?php echo base_url('css/BookBlock/bookblock.css');?>">
-  <link rel="stylesheet" href="<?php echo base_url('css/BookBlock/demo1.css');?>">
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-  <!-- Script de BookBlock el puglin para la animacion a continuacion-->
-  <script src="<?php echo base_url('js\BookBlock/modernizr.custom.js');?>"></script>
-
-  <script src="<?php echo base_url('js\BookBlock/jquerypp.custom.js');?>"></script>
-  <script src="<?php echo base_url('js\BookBlock/bookblock.js');?>"></script>
-  <script>
-  </script>
-</head>
-
-<body>
-  <?php 
-  //Esto cuenta los archivos de una carpeta
-  $total_imagenes = count(glob('assets/libros/12/{*.jpg,*.gif,*.png}',GLOB_BRACE));
-  echo "total_imagenes = ".$total_imagenes;
-  
-  ?>
-  <p>Hola soy la vista del Visor</p>
-  <div class="main clearfix">
-				<div class="bb-custom-wrapper">
-					  <h3>Paginas del libro tal</h3>
-					  <div id="bb-bookblock" class="bb-bookblock">
-						    <div class="bb-item">
-                  <img class='pag2' src="<?php echo base_url('assets/libros/12/1.jpg');?>" /> 
-						    </div>
-						    <div class="bb-item" >
-                <img class='pag3' src="<?php echo base_url('assets/libros/12/2.jpg');?>" /> 
-						    </div>
-						    <div class="bb-item" >
-						     
-						    </div>
-						    <div class="bb-item">
-							    
-						    </div>
-						    <div class="bb-item">
-							    
-						    </div>
-				      </div>
-					<nav>
-						<a id="bb-nav-first" href="#" class="bb-custom-icon bb-custom-icon-first">First page</a>
-						<a id="bb-nav-prev" href="#" class="bb-custom-icon bb-custom-icon-arrow-left">Previous</a>
-						<a id="bb-nav-next" href="#" class="bb-custom-icon bb-custom-icon-arrow-right">Next</a>
-						<a id="bb-nav-last" href="#" class="bb-custom-icon bb-custom-icon-last">Last page</a>
-					</nav>
-				</div>
-  </div>
-
-  <script>
+<html class="no-js demo-1">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/BookBlock/default.css');?>" />
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/BookBlock/bookblock.css');?>"/>
+		<!-- custom demo style -->
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/BookBlock/demo1.css');?>" />
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/visor.css');?>" />
+		
+		
+		<script src="<?php echo base_url('js/BookBlock/modernizr.custom.js');?>"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="<?php echo base_url('js/BookBlock/jquerypp.custom.js');?>" ></script>
+		<script src="<?php echo base_url('js/BookBlock/jquery.bookblock.js');?>" ></script>
+		<script src="<?php echo base_url('js/wheelzoom.js');?>"></script>
+		<script>
+ 		$( document ).ready(function() {
+			wheelzoom(document.querySelectorAll('img'));
+			pagina=1;
+			paginaTotal= document.querySelectorAll('img').length;
+			
 			var Page = (function() {
 				var config = {
 						$bookBlock : $( '#bb-bookblock' ),
@@ -69,7 +32,7 @@
 					},
 					init = function() {
 						config.$bookBlock.bookblock( {
-							speed : 800,
+							speed : 700,
 							shadowSides : 0.8,
 							shadowFlip : 0.7
 						} );
@@ -79,31 +42,52 @@
 						
 						var $slides = config.$bookBlock.children();
 
-						// add navigation events
+						// Añadimos eventos de navegacion Siguiente,Anterior,Primera y ultima pagina
 						config.$navNext.on( 'click touchstart', function() {
-							config.$bookBlock.bookblock( 'next' );
-							return false;
+							nodo="img.zoom"+pagina;
+							if(pagina!=paginaTotal){
+								document.querySelector(nodo).dispatchEvent(new CustomEvent('wheelzoom.reset'));
+								pagina++;
+								config.$bookBlock.bookblock( 'next' );
+								return false;
+							}else{
+								document.querySelector(nodo).dispatchEvent(new CustomEvent('wheelzoom.reset'));
+								config.$bookBlock.bookblock( 'next' );
+							}
+							
 						} );
 
 						config.$navPrev.on( 'click touchstart', function() {
-							config.$bookBlock.bookblock( 'prev' );
-							return false;
+							nodo="img.zoom"+pagina;
+							if(pagina!=1){
+								document.querySelector(nodo).dispatchEvent(new CustomEvent('wheelzoom.reset'));
+								pagina--;
+								config.$bookBlock.bookblock( 'prev' );
+								return false;
+							}else{
+								document.querySelector(nodo).dispatchEvent(new CustomEvent('wheelzoom.reset'));
+								config.$bookBlock.bookblock( 'prev' );
+							}
+							
 						} );
 
 						config.$navFirst.on( 'click touchstart', function() {
+							nodo="img.zoom"+pagina;
+							document.querySelector(nodo).dispatchEvent(new CustomEvent('wheelzoom.reset'));
+							pagina=1;
 							config.$bookBlock.bookblock( 'first' );
 							return false;
 						} );
 
 						config.$navLast.on( 'click touchstart', function() {
+							nodo="img.zoom"+pagina;
+							document.querySelector(nodo).dispatchEvent(new CustomEvent('wheelzoom.reset'));
+							pagina=paginaTotal;
 							config.$bookBlock.bookblock( 'last' );
 							return false;
 						} );
-						
-						// add swipe events
-						
-
-						// add keyboard events
+			
+						// Añadimos Eventos de tecla
 						$( document ).keydown( function(e) {
 							var keyCode = e.keyCode || e.which,
 								arrow = {
@@ -127,11 +111,51 @@
 					return { init : init };
 
 			})();
-		</script>
-		<script>
-				Page.init();
-		</script>
-</body>
+			
+			Page.init();
+
+	
+			
+
+
+		});
+ 
+  </script>
+	</head>
+	<body>
+	
+		<div class="container">
+			<header>
+				<h1>Nombre del Libro a elegir</h1>	
+			</header>
+			<div class="main clearfix">
+				<div class="bb-custom-wrapper">
+					<div id="bb-bookblock" class="bb-bookblock">
+						<div class="bb-item">
+							<img class='zoom1' id="m"  src="<?php echo base_url('assets/libros/12/1.jpg');?>" /> 
+						</div>
+						<div class="bb-item" >
+							<img class='zoom2' src="<?php echo base_url('assets/libros/12/2.jpg');?>" /> 
+						</div>
+						<div class="bb-item" >
+							<img class='zoom3' src="<?php echo base_url('assets/libros/12/3.jpg');?>" /> 
+						</div>
+						<div class="bb-item" >
+							<img class='zoom4' src="<?php echo base_url('assets/libros/12/5.jpg');?>" /> 
+						</div>
+					</div>
+					<nav>
+						<a id="bb-nav-first" href="#" class="bb-custom-icon bb-custom-icon-first">First page</a>
+						<a id="bb-nav-prev" href="#" class="bb-custom-icon bb-custom-icon-arrow-left">Previous</a>
+						<!--BOTON DE PAGINA ACTUAL<input  type='text' id='numeropag' value="2">-->
+						<!--BOTON DE PAGINA TOTAL<input  type='text' value='7' readonly>-->
+						<a id="bb-nav-next" href="#" class="bb-custom-icon bb-custom-icon-arrow-right">Next</a>
+						<a id="bb-nav-last" href="#" class="bb-custom-icon bb-custom-icon-last">Last page</a>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
 <!--
   Asi se encuentra la ruta de los libros en nuestro server
