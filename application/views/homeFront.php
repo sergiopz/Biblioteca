ff8a56a2ef99d53e74a674367e4c61693ac1e7ae
graciancristales <graciancristales@hotmail.com>
2019-02-26 10:44:27 +0100

    ultimos libros

--- application/views/homeFront.php
+++ application/views/homeFront.php
@@ -147,14 +147,18 @@
 <content class="">
   <div class="row"><p></p></div>
     <h2 id="lastBooks" class="display-1 text-center">Últimos Libros</h2>
-      <section class="slider1 slider">
-        <div class="slide"><img clas="imagen" src="https://i.pinimg.com/originals/54/fc/91/54fc9110db41a427b521dde284c5155a.jpg"></div>
-        <div class="slide"><img clas="imagen" src="https://i.pinimg.com/originals/0d/ee/e7/0deee7aa20d17c45c51bc68eb72f381a.jpg"></div>
-        <div class="slide"><img clas="imagen" src="https://i.pinimg.com/originals/c1/6d/27/c16d2725fb61af9b1a0f20b9a8018568.jpg"></div>
-        <div class="slide"><img clas="imagen" src="http://imagenes.publico.es/publico_es/publicolibros/images/libros/211/9788427119604.jpg"></div>
-        <div class="slide"><img clas="imagen" src="http://imagenes.publico.es/publico_es/publicolibros/images/libros/211/9788427119604.jpg"></div>
-        <div class="slide"><img clas="imagen" src="http://3.bp.blogspot.com/_51Xw1f4V4L4/SZufv1kGdaI/AAAAAAAAAH8/KFpp4McknLA/s400/Picture+3.png"></div>
-        <div class="slide"><img clas="imagen" src="http://imagenes.publico.es/publico_es/publicolibros/images/libros/211/9788427119604.jpg"></div>
+       <section class="slider1 slider">
+
+         <?php
+         for ($i = 0; $i < count($ultimosLibros); $i++) {
+          $libro = $ultimosLibros[$i];
+
+          echo"
+        <div class='slide'><a href='".site_url("Buscador/Visor/$libro->id")."'><img clas='imagen' src='".base_url("assets/libros/".$libro->id."/0.jpg")."' ></a></div>";
+      }
+        
+          ?>
+        
       </section>
   <div class="row"><p></p></div>
 </content>
